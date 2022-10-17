7d26ed9259b99cdbbd7c08809a3e96645f351a35
Fiifi <mac@macs-MacBook-Pro.local>
2022-10-17 13:53:09 +0000

    changed page name to justify start

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,36 +1,55 @@
-import React, { Component } from 'react'
-import Menu from './Menu';
+import React, { Component } from "react";
+import Menu from "./Menu";
 
 export class Navbar extends Component {
   constructor(props) {
     super(props);
 
-    this.navbar = React.createRef()
-    
+    this.navbar = React.createRef();
+
     this.state = {
-      showMenu: false
-    }
+      showMenu: false,
+    };
   }
 
   toggleMenu = () => {
-    this.setState({showMenu: !this.state.showMenu})
-    return
-  }
-  
-  
+    this.setState({ showMenu: !this.state.showMenu });
+    return;
+  };
+
   render() {
     return (
       <nav ref={this.navbar} className="navbar general-padding">
-        <span onClick={() => {this.toggleMenu()}} className="material-symbols-outlined menu" style={{cursor: 'pointer'}}>
-            { this.state.showMenu ?  "close" : "menu"}
+        <span
+          onClick={() => {
+            this.toggleMenu();
+          }}
+          className="material-symbols-outlined menu"
+          style={{ cursor: "pointer" }}
+        >
+          {this.state.showMenu ? "close" : "menu"}
         </span>
-        <h3 className='h1-mobile'>{this.props.page}</h3>
-        <span className="avatar material-symbols-outlined" style={{fontSize: '2rem', color: '#b6b6b6',  display: 'grid', justifyItems: 'end', alignItems: 'center'}}> account_circle </span>
-        { this.state.showMenu ? <Menu></Menu> : null }
+        <h3 className="h1-mobile" style={{justifySelf: 'start'}}>{this.props.page}</h3>
+        <span
+          className="avatar material-symbols-outlined"
+          style={{
+            fontSize: "2rem",
+            color: "#212529",
+            display: "grid",
+            justifyItems: "end",
+            alignItems: "center",
+            cursor: "pointer",
+          }}
+        >
+          {" "}
+          account_circle{" "}
+        </span>
+        {this.state.showMenu ? <Menu></Menu> : null}
       </nav>
-    )
+    );
   }
 }
+// show image if the user has already uploaded it
 /* <img className='avatar' src='' alt="" /> */
 
-export default Navbar
+export default Navbar;

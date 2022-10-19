f6a02a56728ec8ce7105c4e80270fdd4b024afa2
Fiifi <mac@macs-MacBook-Pro.local>
2022-10-19 00:05:35 +0000

    done adding markup for the upcoming events accordion

--- src/Components/UpcomingEventDetail.jsx
+++ src/Components/UpcomingEventDetail.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
+import { secondaryColor } from "../Utils/colors";
 
 export class UpcomingEventDetail extends Component {
   render() {
     return (
-      <div className="grid pd-v-max" style={{height: 'auto', gridTemplateColumns: 'auto 1fr', gridTemplateRows: '19px 19px', gridGap: '10.5px'}}>
-        <span className="material-symbols-outlined">calendar_month</span>
-        <div className="asc grid">
-            <h4 style={{fontSize: '14px'}}>Start Date</h4>
+      <div className="grid pd-v-max" style={{ minHeight: '75px', gridTemplateColumns: 'auto 1fr', gridTemplateRows: '19px auto', gridGap: '10.5px'}}>
+        <span className="material-symbols-outlined" style={{color: '#9B9B9B'}}>{this.props.icon}</span>
+        <div className=" grid" style={{gridTemplateColumns: '100px', gridAutoColumns: '100px', gridAutoFlow: 'column', alignItems: 'center', color: '#3C3C3C' }}>
+            <h4 style={{fontSize: '14px'}}>{this.props.detailTitleOne}</h4>
+            <h4 style={{fontSize: '14px'}}>{this.props.detailTitleTwo}</h4>
+        </div>
+
+        <div className=" grid" style={{gridTemplateColumns: '100px', gridAutoColumns: '100px', gridAutoFlow: 'column', alignItems: 'center', gridColumn: '2/3', color: secondaryColor }}>
+            <h4 style={{fontSize: '14px', color: secondaryColor}}>{this.props.detailOne}</h4>
+            <h4 style={{fontSize: '14px'}}>{this.props.detailTwo}</h4>
         </div>
       </div>
     );
--- src/Components/UpcomingEvents.jsx
+++ src/Components/UpcomingEvents.jsx
@@ -1,18 +1,49 @@
 import React, { Component } from "react";
 import UpcomingEventDetail from "./UpcomingEventDetail";
-import image from '../Assets/upcomingEvent.png'
+import image from "../Assets/upcomingEvent.jpeg";
 
 export class UpcomingEvents extends Component {
   render() {
     return (
-      <div className="upcoming-event-container br mg-top mg-down">
+      <div className="upcoming-event-container br mg-top mg-down" >
         <img className="upcoming-event-image" src={image} alt="" />
-        <div className="upcoming-event-name-container " style={{borderBottom: '1px solid #E5E5E5'}}>
+        <div
+          className="upcoming-event-name-container "
+          style={{ borderBottom: "1px solid #E5E5E5" }}
+        >
           <p className="upcoming-event-name pd-h-min">Lorem, ipsum dolor.</p>
         </div>
         <div className="upcoming-event-content pd-h-min">
-            <UpcomingEventDetail></UpcomingEventDetail>
-          </div>
+          <UpcomingEventDetail
+            icon="calendar_month"
+            detailTitleOne="Start Date"
+            detailTitleTwo="End Date"
+            detailOne="2022-05-22"
+            detailTwo="2022-05-22"
+          ></UpcomingEventDetail>
+          <UpcomingEventDetail
+            icon="schedule"
+            detailTitleOne="Start Time"
+            detailTitleTwo="End Time"
+            detailOne="09:00 AM"
+            detailTwo="12:00 PM"
+          ></UpcomingEventDetail>
+          <UpcomingEventDetail
+            icon="location_on"
+            detailTitleOne="Location"
+            detailOne="La Palm Royal Beach Hotel, GhanaM"
+          ></UpcomingEventDetail>
+          <UpcomingEventDetail
+            icon="event_available"
+            detailTitleOne="Event Type"
+            detailOne="Conference"
+          ></UpcomingEventDetail>
+          <UpcomingEventDetail
+            icon="folder"
+            detailTitleOne="Category"
+            detailOne="Business & Professional"
+          ></UpcomingEventDetail>
+        </div>
       </div>
     );
   }
